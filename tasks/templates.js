5243a57c95fd1a6604aaf55de53c59f421602939
Jonathan Clem <jonathan@jclem.net>
2013-05-23 08:57:35 -0700

    Initial commit

---
+++ tasks/templates.js
@@ -0,0 +1,45 @@
+/*
+ * grunt-simple-templates
+ * https://github.com/jclem/grunt-simple-templates
+ *
+ * Copyright (c) 2013 Jonathan Clem
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var fs    = require('fs'),
+    path  = require('path');
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('templates', 'Render templates into JavaScript objects', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', ',
+      namespace: 'TEMPLATES'
+    });
+
+    var templateObject = {};
+
+    this.filesSrc.forEach(function(file) {
+      grunt.file.recurse(file, function(absPath, rootDir, subdir, filename) {
+        var templateKey = filename.split('.')[0];
+
+        if (subdir) {
+          templateKey = subdir + "/" + templateKey;
+        }
+
+        templateObject[templateKey] = grunt.file.read(absPath);
+      });
+    });
+
+    grunt.file.write(this.data.dest, "window." + options.namespace + " = " + JSON.stringify(templateObject) + ";\n");
+    grunt.log.writeln('File "' + this.data.dest + '" created.');
+  });
+
+};
